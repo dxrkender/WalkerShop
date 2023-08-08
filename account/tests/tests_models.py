d9f6f6df8d243a665d47e843cbbdd4c0e5ec4acf
TarhanovM <tarmd@ya.ru>
2023-08-08 23:26:41 +0500

    add test for account app

---
+++ account/tests/tests_models.py
@@ -0,0 +1,63 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from account.models import Client
+
+
+class TestClient(TestCase):
+
+    def setUp(self) -> None:
+        self.test_client1 = Client.objects.create_user(
+            username='TestClient1',
+            email='test@client.com',
+            is_active=True,
+        )
+        self.test_client2 = Client.objects.create_user(
+            username='ТестКлиент2',
+            email='test2@client.com',
+            is_active=True,
+        )
+        self.test_client1.save()
+        self.test_client2.save()
+
+    def test_save(self):
+        test_client1 = Client.objects.get(pk=self.test_client1.pk)
+        self.assertEqual(test_client1.slug, 'testclient1')
+
+    def test_verbose_name(self):
+        test_client1 = Client.objects.get(pk=self.test_client1.pk)
+
+        username_verbose_name = test_client1._meta.get_field('username').verbose_name
+        email_verbose_name = test_client1._meta.get_field('email').verbose_name
+        slug_verbose_name = test_client1._meta.get_field('slug').verbose_name
+        is_active_verbose_name = test_client1._meta.get_field('is_active').verbose_name
+        date_joined_verbose_name = test_client1._meta.get_field('date_joined').verbose_name
+
+        self.assertEqual(username_verbose_name, 'Username')
+        self.assertEqual(email_verbose_name, 'Email')
+        self.assertEqual(slug_verbose_name, 'Slug')
+        self.assertEqual(is_active_verbose_name, 'Activity')
+        self.assertEqual(date_joined_verbose_name, 'Joined at')
+
+    # def test_get_absolute_url(self):
+    #     test_client1 = Client.objects.get(pk=self.test_client1.pk)
+    #     # TODO: White url path with name 'home'
+    #     print(test_client1.get_absolute_url())
+
+    def test_str(self):
+        test_client1 = Client.objects.get(pk=self.test_client1.pk)
+        test_client2 = Client.objects.get(pk=self.test_client2.pk)
+        self.assertEqual(str(test_client1), '<User: TestClient1 - test@client.com - testclient1>')
+        self.assertEqual(str(test_client2), '<User: ТестКлиент2 - test2@client.com - 2>')
+
+    def test_username_validators(self):
+        self.test_client3 = Client.objects.create_user(
+            username='abc',
+            email='abc@abc.com',
+            is_active=True,
+        )
+        with self.assertRaises(ValidationError):
+            self.test_client3.full_clean()
+
+    def tearDown(self) -> None:
+        self.test_client1.delete()
+        self.test_client2.delete()
