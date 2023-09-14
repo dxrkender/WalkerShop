76559e58abf4dd48264f31c84e5b31112b389196
TarhanovM <tarmd@ya.ru>
2023-09-14 21:25:53 +0500

    Remove SECRET_KEY from settings and add getting it from envirionment

--- WalkerShop/settings.py
+++ WalkerShop/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9i%+w3cpny6vmldxb984(_(raqs6vi=a##3heo3)ym398pq3i^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
